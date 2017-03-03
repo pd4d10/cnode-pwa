a3d839168dc494579773bd8c0b8a6b84ac7eb2ed
pd4d10 <pd4d10@gmail.com>
2017-03-03 23:02:45 +0800

    Add Redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import App from './App';
+import reducers from './reducers'
 import './index.css';
 
+const middlewares = [thunk]
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose // eslint-disable-line
+const store = createStore(reducers, composeEnhancers(applyMiddleware(...middlewares)))
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/reducers/list.js
@@ -0,0 +1,3 @@
+export default function list(state, action) {
+  return state
+}
