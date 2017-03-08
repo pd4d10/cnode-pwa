60ef2bc30f1f3e194a1519ce00f61a54e9afe2d1
pd4d10 <pd4d10@gmail.com>
2017-03-08 23:32:27 +0800

    Fix typo

--- src/reducers/message.js
+++ src/reducers/message.js
@@ -7,7 +7,7 @@ export default function message(state = {}, action) {
     case types.LOAD_SUCCESS:
       return state.set('isLoading', false)
         .set('has_read_messages', action.has_read_messages)
-        .set('hasnot_read_messagess', action.hasnot_read_messages)
+        .set('hasnot_read_messages', action.hasnot_read_messages)
     default:
       return state
   }
