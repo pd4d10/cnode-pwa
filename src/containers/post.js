8dbbd6943681708f674361ab2e55f215c0bc19bd
Rongjian Zhang <pd4d10@gmail.com>
2018-10-17 17:34:02 +0800

    feat: post form validation

--- src/containers/post.js
+++ src/containers/post.js
@@ -1,20 +1,13 @@
 import React from 'react'
-import {
-  IconButton,
-  FormControl,
-  Input,
-  InputLabel,
-  Select,
-  MenuItem,
-} from '@material-ui/core'
+import { IconButton, TextField } from '@material-ui/core'
 import { withRouter } from 'react-router-dom'
 import { compose } from 'recompose'
 import { withStyles } from '@material-ui/core/styles'
 import { Send } from '@material-ui/icons'
 import { Header } from '../components'
-import $s from './post.module.css'
 import { fetchAPI } from '../utils'
 import { withContext, AuthConsumer } from '../contexts'
+import $s from './post.module.css'
 
 const postTabs = [
   { id: 'ask', name: '问答' },
@@ -24,11 +17,25 @@ const postTabs = [
 ]
 
 class Post extends React.Component {
-  state = { tab: 'ask', title: '', content: '' }
+  state = {
+    tab: 'ask',
+    title: '',
+    content: '',
+    titleErrorVisible: false,
+    contentErrorVisible: false,
+  }
 
   handleSubmit = async e => {
     e.preventDefault()
-    const id = await this.props.postTopic(this.state)
+    const { tab, title, content } = this.state
+    if (!title || !content) {
+      this.setState({
+        titleErrorVisible: !title,
+        contentErrorVisible: !content,
+      })
+      return
+    }
+    const id = await this.props.postTopic({ tab, title, content })
     this.props.history.replace(`/topic/${id}`)
   }
 
@@ -36,7 +43,14 @@ class Post extends React.Component {
     this.setState({ tab: e.target.value })
   }
 
+  shouldShowTitleError = () => this.state.titleErrorVisible && !this.state.title
+  shouldShowContentError = () =>
+    this.state.contentErrorVisible && !this.state.content
+
   render() {
+    const showTitleError = this.shouldShowTitleError()
+    const showContentError = this.shouldShowContentError()
+
     return (
       <form onSubmit={this.handleSubmit}>
         <Header
@@ -48,50 +62,60 @@ class Post extends React.Component {
           )}
         />
         <div style={{ padding: 10 }}>
-          <FormControl fullWidth className={$s.control}>
-            <InputLabel htmlFor="title">标题</InputLabel>
-            <Input
-              id="title"
-              value={this.state.title}
-              onChange={e => {
+          <TextField
+            fullWidth
+            className={$s.control}
+            label="标题"
+            InputLabelProps={{ htmlFor: 'title' }}
+            InputProps={{
+              id: 'title',
+              value: this.state.title,
+              onChange: e => {
                 this.setState({ title: e.target.value })
-              }}
-            />
-          </FormControl>
-          <FormControl fullWidth className={$s.control}>
-            <InputLabel htmlFor="tab">分区</InputLabel>
-            <Select
-              native
-              id="tab"
-              value={this.state.tab}
-              onChange={this.handleTabChange}
-              // input={
-              //   <OutlinedInput
-              //     labelWidth={this.state.labelWidth}
-              //     name="age"
-              //     id="outlined-age-simple"
-              //   />
-              // }
-            >
-              {postTabs.map(({ id, name }) => (
-                <option key={id} value={id}>
-                  {name}
-                </option>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl fullWidth className={$s.control}>
-            <InputLabel htmlFor="content">内容</InputLabel>
-            <Input
-              multiline
-              rows="10"
-              id="content"
-              value={this.state.content}
-              onChange={e => {
+              },
+              error: showTitleError,
+            }}
+            error={showTitleError}
+            helperText={showTitleError ? '请输入标题' : null}
+          />
+          <TextField
+            fullWidth
+            className={$s.control}
+            select
+            label="分区"
+            id="tab"
+            SelectProps={{
+              native: true,
+              id: 'tab',
+              value: this.state.tab,
+              onChange: this.handleTabChange,
+            }}
+          >
+            {postTabs.map(({ id, name }) => (
+              <option key={id} value={id}>
+                {name}
+              </option>
+            ))}
+          </TextField>
+          <TextField
+            // TODO: markdown
+            multiline
+            rows="10"
+            fullWidth
+            className={$s.control}
+            label="内容"
+            InputLabelProps={{ htmlFor: 'content' }}
+            InputProps={{
+              id: 'content',
+              value: this.state.content,
+              onChange: e => {
                 this.setState({ content: e.target.value })
-              }}
-            />
-          </FormControl>
+              },
+              error: showContentError,
+            }}
+            error={showContentError}
+            helperText={showContentError ? '请输入内容' : null}
+          />
         </div>
         {/* <MdEditor
           value={this.state.content}
